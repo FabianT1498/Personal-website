086c941ef67dceadc9a1f5994eeed64bf32f6733
Fabian Trillo <fabiantrillo1498@gmail.com>
2024-11-08 11:05:29 -0400

    added personal info

--- script.js
+++ script.js
@@ -72,36 +72,6 @@ navbar.addEventListener('blur', (event) => {
 
 navbar.setAttribute('tabindex', '0'); // Make it focusable
   
-  //   $(".header ul li a").click(function(e) {
-  //     e.preventDefault(); 
-  
-  //     var target = $(this).attr("href");
-  
-  //     if ($(target).hasClass("active-section")) {
-  //       return; 
-  //     }
-  
-  //     if (target === "#home") {
-  //       $("html, body").animate(
-  //         {
-  //           scrollTop: 0 
-  //         },
-  //         500
-  //       );
-  //     } else {
-  //       var offset = $(target).offset().top - 40; 
-  
-  //       $("html, body").animate(
-  //         {
-  //           scrollTop: offset
-  //         },
-  //         500
-  //       );
-  //     }
-  
-  //     $(".header ul li a").removeClass("active");
-  //     $(this).addClass("active");
-  //   });
   
 
   //   //Initial content revealing js
